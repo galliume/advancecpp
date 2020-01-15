c3e45fbf21eec72ab542426984b764c87e90d87d
Guillaume Percepied <gcconantc@gmail.com>
2020-01-15 16:41:55 +0100

    Exceptions

--- exceptions.cpp
+++ exceptions.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <exception>
+
 using namespace std;
 
 void mightGoWrong() {
@@ -14,15 +16,52 @@ void mightGoWrong() {
     }
 }
 
+class CanGoWrong {
+    public:
+        CanGoWrong() {
+            char *pMemory= new char[99999999999999999];
+            delete[] pMemory;
+        }
+};
+
+class MyException: public exception {
+    public:
+        virtual const char* what() const throw() {
+            return "MyException !";
+        }
+};
+
+class Test {
+    public:
+        void goesWrong() {
+            throw MyException();
+        }
+        
+};
+
 int main()
 {
+
+    Test test;
+    try {
+        test.goesWrong();
+    } catch (MyException &e) {
+        cout << e.what() << endl;
+    }
+
+    try {
+        CanGoWrong wrong;
+    } catch (bad_alloc &e) {
+        cout << "Caught " << e .what() << endl;
+    }
+
     try {
         mightGoWrong();
     } catch (int e) {
         cout << "Err " << e << endl;
     } catch (char const* e) {
         cout << "Errr" << e << endl;
-    } catch (string e) {
+    } catch (string& e) {
         cout << "Err" << e << endl;
     }
 
